39bb2df0f7d58b08ed302b41438b4bf61bed8ce7
Jimwell Calma <jimwellwork28@gmail.com>
2024-10-26 20:27:36 +0800

    Small Adjustments

--- savoria_vue/src/components/IndexComponent.vue
+++ savoria_vue/src/components/IndexComponent.vue
@@ -80,13 +80,46 @@
   
         <!-- Stats Section -->
         <section id="stats" class="stats section dark-background">
-          <img src="../../public/img/stats-bg.jpg" alt="Stats Background" data-aos="fade-in">
-          <div class="container position-relative" data-aos="fade-up" data-aos-delay="100">
-            <div class="row gy-4">
-              <!-- PureCounter elements here -->
-            </div>
-          </div>
-        </section>
+
+<img src="../../public/img/stats-bg.jpg" alt="" data-aos="fade-in">
+
+<div class="container position-relative" data-aos="fade-up" data-aos-delay="100">
+
+  <div class="row gy-4">
+
+    <div class="col-lg-3 col-md-6">
+      <div class="stats-item text-center w-100 h-100">
+        <span data-purecounter-start="0" data-purecounter-end="232" data-purecounter-duration="1" class="purecounter"></span>
+        <p>Clients</p>
+      </div>
+    </div><!-- End Stats Item -->
+
+    <div class="col-lg-3 col-md-6">
+      <div class="stats-item text-center w-100 h-100">
+        <span data-purecounter-start="0" data-purecounter-end="521" data-purecounter-duration="1" class="purecounter"></span>
+        <p>Projects</p>
+      </div>
+    </div><!-- End Stats Item -->
+
+    <div class="col-lg-3 col-md-6">
+      <div class="stats-item text-center w-100 h-100">
+        <span data-purecounter-start="0" data-purecounter-end="1453" data-purecounter-duration="1" class="purecounter"></span>
+        <p>Hours Of Support</p>
+      </div>
+    </div><!-- End Stats Item -->
+
+    <div class="col-lg-3 col-md-6">
+      <div class="stats-item text-center w-100 h-100">
+        <span data-purecounter-start="0" data-purecounter-end="32" data-purecounter-duration="1" class="purecounter"></span>
+        <p>Workers</p>
+      </div>
+    </div><!-- End Stats Item -->
+
+  </div>
+
+</div>
+
+</section>
   
         <!-- Gallery Section with Swiper -->
         <section id="gallery" class="gallery section light-background">
